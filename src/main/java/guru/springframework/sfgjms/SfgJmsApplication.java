8825fdbdf6ba4f01ce38978804d1942ec26f4f35
niro1982 <n.edinger1982@gmail.com>
2020-07-11 14:13:07 +0300

    send and receive message activeMq in a docker container

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,23 +1,36 @@
 package guru.springframework.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgJmsApplication {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 
-		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+		//This server configuration for embedded activeMQ is actually not needed when having the 2 dependencies
+		//in teh pom, spring will configure it for us
+/*				<dependency>
+			<groupId>org.apache.activemq</groupId>
+			<artifactId>artemis-server</artifactId>
+		</dependency>
+
+		<dependency>
+			<groupId>org.apache.activemq</groupId>
+			<artifactId>artemis-jms-server</artifactId>
+		</dependency>*/
+
+
+
+
+
+/*		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
 			.setPersistenceEnabled(false)
 			.setJournalDirectory("target/data/journal")
 			.setSecurityEnabled(false)
 			.addAcceptorConfiguration("invm", "vm://0"));
 
-		server.start();
+		server.start();*/
 
 		SpringApplication.run(SfgJmsApplication.class, args);
 	}
